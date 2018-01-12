a7be6332c500eed5c09ec12548b47e8e951e7a0d
Austin Albrecht <austin.aa@me.com>
2018-01-12 22:10:44 +0100

    Adds ability to set output limit fixing #1

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ This is a simple script that will fetch the latest message from a Telegram bot a
 
 ## Setup
 1. Clone the repository
-2. Change directory to the repo and then edit the example config file to use your bot token. You do not need to change the `counter` value.
+2. Change directory to the repo and then edit the example config file to use your bot token.
 3. Install dependencies with npm
 4. Run the script
 ```sh
@@ -14,3 +14,9 @@ vim tg-config.json
 npm install
 npm start
 ```
+
+## Config
+The `tg-config.json` has 3 values that can be set:
+- `token`: This is your Telegram bot api token.
+- `charLimit`: This lets you set a maximum lenght for the output (set to `-1` for no limit)
+- `counter`: Which is used to track the last seen Telegram message. You should never need to change this.
--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var conf = require('./tg-config.json')
 
 var url = 'https://api.telegram.org/bot' + conf.token + '/getUpdates'
 
+if (conf.charLimit === undefined) {
+  conf.charLimit = -1
+}
+
 request({url: url, qs: {'limit': 100, 'offset': conf.counter}}, (err, res, body) => {
   if (err) {
     console.log('ERROR')
@@ -29,12 +33,22 @@ function getLastTxtMsg (msgs) {
 
   if (msgs[msgs.length - 1][type].text !== undefined) {
     str = msgs[msgs.length - 1][type].text.split('\n').join(' / ')
-    console.log(str)
+    console.log(limitLength(str))
   } else if (msgs[msgs.length - 1][type].caption !== undefined) {
     str = msgs[msgs.length - 1][type].caption.split('\n').join(' / ')
-    console.log(str)
+    console.log(limitLength(str))
   } else {
     msgs.splice(-1, 1)
     getLastTxtMsg(msgs)
+  }
+}
+
+function limitLength (msg) {
+  if (conf.charLimit <= 0) {
+    return msg
+  } else if (msg.length > conf.charLimit) {
+    return msg.substring(0, conf.charLimit - 4) + '...'
+  } else {
+    return msg
   }
 }
