98a22da1b8bb1a89acce74d3790c6e0d4bc9bbc8
Austin Albrecht <austin.aa@me.com>
2018-01-12 21:48:12 +0100

    adds support for photo captions, potentially fixes issue #2 but am not sure

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ request({url: url, qs: {'limit': 100, 'offset': conf.counter}}, (err, res, body)
   }
   var msgs = JSON.parse(body).result
 
+  conf.counter = msgs[msgs.length - 1].update_id
+  fs.writeFileSync('./tg-config.json', JSON.stringify(conf))
+
   getLastTxtMsg(msgs)
 })
 
@@ -22,11 +25,14 @@ function getLastTxtMsg (msgs) {
     type = 'message'
   }
 
+  var str = ''
+
   if (msgs[msgs.length - 1][type].text !== undefined) {
-    var str = msgs[msgs.length - 1][type].text.split('\n').join(' / ')
+    str = msgs[msgs.length - 1][type].text.split('\n').join(' / ')
     console.log(str)
-    conf.counter = msgs[msgs.length - 1].update_id
-    fs.writeFileSync('./tg-config.json', JSON.stringify(conf))
+  } else if (msgs[msgs.length - 1][type].caption !== undefined) {
+    str = msgs[msgs.length - 1][type].caption.split('\n').join(' / ')
+    console.log(str)
   } else {
     msgs.splice(-1, 1)
     getLastTxtMsg(msgs)
